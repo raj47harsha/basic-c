a6e2e94e1c95ad6f3f15e57d9c240def034e9f2f
raj47harsha <kapadiarajkumar7@gmail.com>
2023-02-14 21:28:28 +0530

    submodule is added

---
+++ c/pattern/fullpyramid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+  int i, j, k, row;
+  printf("enter the number of rows :");
+  scanf("%d", &row);
+  for (i = 1; i <= row; i++)
+  {
+    for (j = 1; j <= 2*i - 1; j++)
+    {
+      printf("*");
+    }
+    for (k = 1; k <= row - i; k++)
+    {
+      printf("");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
