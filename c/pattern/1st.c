9bedbed4f2b70c20cb49cdcba4e50e71b4caa4fa
raj47harsha <kapadiarajkumar7@gmail.com>
2023-02-16 15:42:54 +0530

    1st.c modified

--- c/pattern/1st.c
+++ c/pattern/1st.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
-void main ()
-{int i,j;
-for(i=1;i<=4;i++){
-  for(j=1;j<=5;j++){
-    printf("%d",j);
+int main ()
+
+{int i,j,n;
+printf("enter number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++){
+  for(j=1;j<=n;j++){
+    printf("*");
   }printf("\n");
 }
-
+return 0;
 }
 
